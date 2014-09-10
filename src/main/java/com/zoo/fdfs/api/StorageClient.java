d6ae95defe7051694f28536df0fec1ccd12ad89f
yankai913 <yankai913@gmail.com>
2014-09-10 17:14:42 +0800

    添加StorageClient实现

--- src/main/java/com/zoo/fdfs/api/StorageClient.java
+++ src/main/java/com/zoo/fdfs/api/StorageClient.java
@@ -10,86 +10,138 @@ import java.util.Map;
  */
 public interface StorageClient {
 
-    public TrackerClient getTrackerClient();
+    public String[] uploadFile(String localFileName, String fileExtName, Map<String, String> meta)
+            throws FdfsException;
 
 
-    public FdfsClientConfig getFdfsClientConfig();
+    public String[] uploadFile(byte[] fileBuff, int offset, int length, String fileExtName,
+            Map<String, String> meta) throws FdfsException;
 
 
+    public String[] uploadFile(String groupName, byte[] fileBuff, int offset, int length, String fileExtName,
+            Map<String, String> meta) throws FdfsException;
+
+
+    public String[] uploadFile(byte[] fileBuff, String fileExtName, Map<String, String> meta)
+            throws FdfsException;
+
+
+    public String[] uploadFile(String groupName, byte[] fileBuff, String fileExtName, Map<String, String> meta)
+            throws FdfsException;
+
+
+    public String[] uploadFile(String groupName, long fileSize, UploadCallback callback, String fileExtName,
+            Map<String, String> meta) throws FdfsException;
+
+
+    public String[] uploadFile(String groupName, String masterFileName, String prefixName,
+            String localFileName, String fileExtName, Map<String, String> meta) throws FdfsException;
+
+
+    public String[] uploadFile(String groupName, String masterFileName, String prefixName, byte[] fileBuff,
+            String fileExtName, Map<String, String> meta) throws FdfsException;
+
+
+    public String[] uploadFile(String groupName, String masterFileName, String prefixName, byte[] fileBuff,
+            int offset, int length, String fileExtName, Map<String, String> meta) throws FdfsException;
+
+
+    public String[] uploadFile(String groupName, String masterFileName, String prefixName, long fileSize,
+            UploadCallback callback, String fileExtName, Map<String, String> meta) throws FdfsException;
+
+
+    public String[] uploadAppenderFile(String localFileName, String fileExtName, Map<String, String> meta)
+            throws FdfsException;
+
+
+    public String[] uploadAppenderFile(byte[] fileBuff, int offset, int length, String fileExtName,
+            Map<String, String> meta) throws FdfsException;
+
+
+    public String[] uploadAppenderFile(String groupName, byte[] fileBuff, int offset, int length,
+            String fileExtName, Map<String, String> meta) throws FdfsException;
+
+
+    public String[] uploadAppenderFile(byte[] fileBuff, String fileExtName, Map<String, String> meta)
+            throws FdfsException;
+
+
+    public String[] uploadAppenderFile(String groupName, byte[] fileBuff, String fileExtName,
+            Map<String, String> meta) throws FdfsException;
+
+
+    public String[] uploadAppenderFile(String groupName, long fileSize, UploadCallback callback,
+            String fileExtName, Map<String, String> meta) throws FdfsException;
+
+
+    public int appendFile(String groupName, String appenderFileName, String localFileName)
+            throws FdfsException;
+
+
+    public int appendFile(String groupName, String appenderFileName, byte[] fileBuff) throws FdfsException;
+
+
+    public int appendFile(String groupName, String appenderFileName, byte[] fileBuff, int offset, int length)
+            throws FdfsException;
+
+
+    public int appendFile(String groupName, String appenderFileName, long fileSize, UploadCallback callback)
+            throws FdfsException;
+
+
+    public int modifyFile(String groupName, String appenderFileName, long fileOffset, String localFileName)
+            throws FdfsException;
+
+
+    public int modifyFile(String groupName, String appenderFileName, long fileOffset, byte[] fileBuff)
+            throws FdfsException;
+
+
+    public int modifyFile(String groupName, String appenderFileName, long fileOffset, byte[] fileBuff,
+            int bufferOffset, int bufferLength) throws FdfsException;
+
+
+    public int modifyFile(String groupName, String appenderFileName, long fileOffset, long modifySize,
+            UploadCallback callback) throws FdfsException;
+
+
+    public int truncateFile(String groupName, String appenderFileName) throws FdfsException;
+
+
+    public int truncateFile(String groupName, String appenderFileName, long truncatedFileSize)
+            throws FdfsException;
+
+
+    public FileInfo getFileInfo(String groupName, String remoteFileName) throws FdfsException;
+
+
+    public FileInfo queryFileInfo(String groupName, String remoteFileName) throws FdfsException;
+
+
+    public Map<String, String> getMetadata(String groupName, String remoteFileName) throws FdfsException;
     
-    public String[] uploadFile(String localFileName, String fileExtName, Map<String, String> meta);
-    public String[] uploadFile(String localFileName, String fileExtName, Map<String, String> meta, String groupName);
-            
-
-    public String[] uploadFile(byte[] fileBuff, int offset, int length, String fileExtName, Map<String, String> meta);
-    public String[] uploadFile(byte[] fileBuff, int offset, int length, String fileExtName, Map<String, String> meta, String groupName);
-            
-
-    public String[] uploadFile(byte[] fileBuff, String fileExtName, Map<String, String> meta);
-    public String[] uploadFile(byte[] fileBuff, String fileExtName, Map<String, String> meta, String groupName);
 
 
-    public String[] uploadFile(String groupName, String masterFileName, String prefixName, String localFileName, String fileExtName, Map<String, String> meta);
-    public String[] uploadFile(String groupName, String masterFileName, String prefixName, byte[] fileBuff, String fileExtName, Map<String, String> meta);
-    public String[] uploadFile(String groupName, String masterFileName, String prefixName, byte[] fileBuff, int offset, int length, String fileExtName, Map<String, String> meta);
-    
-    //callback
-    public String[] uploadFile(String groupName, String masterFileName, String prefixName, long fileSize, UploadCallback callback, String fileExtName, Map<String, String> meta);
-    
-
-    
-    public String[] uploadAppenderFile(String localFileName, String fileExtName, Map<String, String> meta);
-    public String[] uploadAppenderFile(String localFileName, String fileExtName, Map<String, String> meta, String groupName);
-
-    
-    public String[] uploadAppenderFile(byte[] fileBuff, int offset, int length, String fileExtName, Map<String, String> meta);
-    public String[] uploadAppenderFile(byte[] fileBuff, int offset, int length, String fileExtName, Map<String, String> meta, String groupName);
-            
-
-    public String[] uploadAppenderFile(byte[] fileBuff, String fileExtName, Map<String, String> meta);
-    public String[] uploadAppenderFile(byte[] fileBuff, String fileExtName, Map<String, String> meta, String groupName);
-            
-    public String[] uploadAppenderFile(String groupName, long fileSize, UploadCallback callback, String fileExtName, Map<String, String> meta);
-    
-    
-    
-    public int appendFile(String groupName, String appenderFileName, String localFileName);
-    public int appendFile(String groupName, String appenderFileName, byte[] fileBuff);
-    public int appendFile(String groupName, String appenderFilename, byte[] fileBuff, int offset, int length);
-    public int appendFile(String groupName, String appenderFileName, long fileSize, UploadCallback callback);
-
-    
-
-    public int modifyFile(String groupName, String appenderFileName, long fileOffset, String localFileName);
-    public int modifyFile(String groupName, String appenderFileName, long fileOffset, byte[] fileBuff);
-    public int modifyFile(String groupName, String appenderFileName, long fileOffset, byte[] fileBuff, int bufferOffset, int bufferLength);
-    public int modifyFile(String groupName, String appenderFileName, long fileOffset, long modifySize, UploadCallback callback);
+    public byte[] downloadFile(String groupName, String remoteFileName) throws FdfsException;
 
 
-    public int deleteFile(String groupName, String remoteFileName);
-    
-    
-    public int truncateFile(String groupName, String appenderFileName);
-    public int truncateFile(String groupName, String appenderFileName, long truncatedFileSize);
+    public byte[] downloadFile(String groupName, String remoteFileName, long fileOffset, long downloadBytes)
+            throws FdfsException;
 
 
-    public byte[] downloadFile(String groupName, String remoteFileName);
-    public byte[] downloadFile(String groupName, String remoteFileName, long fileOffset, long downloadBytes);
+    public int downloadFile(String groupName, String remoteFileName, String localFileName)
+            throws FdfsException;
 
 
-    public int downloadFile(String groupName, String remoteFileName, String localFileName);
-    public int downloadFile(String groupName, String remoteFileName, String localFileName, long fileOffset, long downloadBytes);
-            
+    public int downloadFile(String groupName, String remoteFileName, String localFileName, long fileOffset,
+            long downloadBytes) throws FdfsException;
 
 
-    public int downloadFile(String groupName, String remoteFileName, DownloadCallback callback);
-    public int downloadFile(String groupName, String remoteFileName, DownloadCallback callback, long fileOffset, long downloadBytes);
+    public int downloadFile(String groupName, String remoteFileName, DownloadCallback callback)
+            throws FdfsException;
 
 
-    public Map<String, String> getMetadata(String groupName, String remoteFileName);
-    public int setMetadata(String groupName, String remoteFileName, Map<String, String> meta, byte opFlag);
-            
+    public int downloadFile(String groupName, String remoteFileName, DownloadCallback callback,
+            long fileOffset, long downloadBytes) throws FdfsException;
 
-    public FileInfo getFileInfo(String groupName, String remoteFileName);
-    public FileInfo queryFileInfo(String groupName, String remoteFileName);
 }
